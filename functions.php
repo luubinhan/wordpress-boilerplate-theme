67f2af2cdda7cdfed991a514a5967ad57933f2d2
Luu Binh An <luubinhan1989@gmail.com>
2014-05-21 11:49:00 +0700

    no message

--- functions.php
+++ functions.php
@@ -122,20 +122,6 @@ function admin_color_scheme() {
 add_action('admin_head', 'admin_color_scheme');
 
 
-/* REPLACE HOWDY ADMIN
--------------------------------------------------------------- */
-
-function replace_howdy( $wp_admin_bar ) {
-    $my_account=$wp_admin_bar->get_node('my-account');
-    $newtitle = str_replace( 'Howdy,', 'Chào bạn,', $my_account->title );           
-    $wp_admin_bar->add_node( array(
-        'id' => 'my-account',
-        'title' => $newtitle,
-    ) );
-}
-add_filter( 'admin_bar_menu', 'replace_howdy',25 );
-
-
 /* CHANG DESKBOAR FOOTER
 -------------------------------------------------------------- */
 
