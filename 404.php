27208a1bc2bf71645bff4b63b8941d71c3116b4b
Luu Binh An <luubinhan1989@gmail.com>
2016-05-19 11:07:36 +0700

    no message

--- 404.php
+++ 404.php
@@ -5,9 +5,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <?php get_header(); ?>
 
-  <div id="main" role="main">
-      <h1>Not found</h1>
-      <p><span frown>:(</span></p>
-  </div>
+  <div class="boo-wrapper">
+    <div class="boo-name">
+    	<span class="boo-hidden">
+    		404
+    	</span>
+    </div>
+    <h1>Whoops!</h1>
+    <p>
+      We couldn't find the page you
+      <br />
+      were looking for.
+    </p>
+    <div class="align-center">
+    	<a href="<?php echo get_option('home'); ?>/" class="btn btn-light"><?php _e("Back to home","dev" ); ?></a>
+    </div>
+</div><!-- boo-wrapper -->
 
 <?php get_footer(); ?>
